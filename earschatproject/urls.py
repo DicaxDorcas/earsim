2b51d0f4a04b569825a00e1e81f6d591441ec832
DicaxDorcas <github@rosswalker.me>
2013-12-01 19:20:31 +0000

    Fix

--- earschatproject/urls.py
+++ earschatproject/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 admin.autodiscover()
 
@@ -7,6 +8,7 @@ urlpatterns = patterns('',
     url(r'^logout/$', 'django.contrib.auth.views.logout_then_login', name='logout'),
     url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
 
+    url(r'^manifest.webapp$', TemplateView.as_view(template_name='manifest.webapp')),
     url(r'^', include('chat.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
