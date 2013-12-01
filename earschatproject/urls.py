ce3d47ffec9a282d9d49d7f915634096b55a2ee0
DicaxDorcas <github@rosswalker.me>
2013-12-01 19:26:51 +0000

    Fixed up manifest

--- earschatproject/urls.py
+++ earschatproject/urls.py
@@ -8,7 +8,7 @@ urlpatterns = patterns('',
     url(r'^logout/$', 'django.contrib.auth.views.logout_then_login', name='logout'),
     url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
 
-    url(r'^manifest.webapp$', TemplateView.as_view(template_name='manifest.webapp')),
+    url(r'^manifest.webapp$', TemplateView.as_view(content_type='application/x-web-app-manifest+json', template_name='manifest.webapp')),
     url(r'^', include('chat.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
